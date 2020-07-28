41e627a4f94b3b959e8b4388ec1c4935455749ca
PatrySNCHZ <ricchustorm@gmail.com>
2020-07-28 23:26:07 +0200

    Update CarritoController.php stdClass object error solved
    Error in $_SESSION['carrito'][] = array
    Typo ERR didnt put [] behind

--- controllers/CarritoController.php
+++ controllers/CarritoController.php
@@ -2,65 +2,60 @@
 
 require_once 'models/producto.php';
 
-class carritoController{
-        public function index(){
-            
-            var_dump($_SESSION['carrito']);
-            
-            echo "Controlador carrito, acción index";
+class carritoController {
+
+    public function index() {
+
+        var_dump($_SESSION['carrito']);
+
+        echo "Controlador carrito, acción index";
+    }
+
+    public function add() {
+
+        if (isset($_GET['id'])) {
+            $producto_id = $_GET['id'];
+        } else {
+            header('Location' . base_url);
         }
-        
-        
-        public function add(){
-            
-            if(isset($_GET['id'])){
-                $producto_id = $_GET['id'];
-                
-            }else{
-                header('Location'.base_url);
-            }
-            
-            if(isset($_SESSION['carrito'])){
-                $counter = 0;
-                foreach($_SESSION['carrito'] as $indice => $elemento){
-                    if($elemento['id_producto'] == $producto_id){
-                        $_SESSION['carrito'][$indice]['unidades']++;
-                        $counter++;
-                    }
+
+        if (isset($_SESSION['carrito'])) {
+            $counter = 0;
+            foreach ($_SESSION['carrito'] as $indice => $elemento) {
+                if ($elemento['id_producto'] == $producto_id) {
+                    $_SESSION['carrito'][$indice]['unidades'] ++;
+                    $counter++;
                 }
-                
             }
-            
-            if(!isset($counter) || $counter == 0){
-                //Conseguir producto
-                $producto = new Producto();
-                $producto->setId($producto_id);
-                $producto = $producto->getOne();
-                
-                // Añadir producto al carrito
-                if(is_object($producto)){
-                $_SESSION['carrito']=array (
-                    "id_producto" => $producto->id,
-                    "precio" => $producto->precio,
-                    "unidades" => 1,
-                    "producto"=> $producto
+        }
+
+        if (!isset($counter) || $counter == 0) {
+            //Conseguir producto
+            $producto = new Producto();
+            $producto->setId($producto_id);
+            $producto = $producto->getOne();
+
+            // Añadir producto al carrito
+            if (is_object($producto)) {
+                $_SESSION['carrito'][] = array(
+                    'id_producto' => $producto->id,
+                    'precio' => $producto->precio,
+                    'unidades' => 1,
+                    'producto' => $producto
                 );
-                
-                }
             }
-            
-            header("Location:".base_url."carrito/index");
-            
         }
+
+        header("Location:" . base_url . "carrito/index");
+    }
+
+    public function remove() {
         
-        public function remove(){
-            
-        }
-        
-        public function delete_all(){
-            unset($_SESSION['carrito']);
-            header("Location:".base_url."carrito/index");
-        }
-        
-        
-        }
+    }
+
+    public function delete_all() {
+        unset($_SESSION['carrito']);
+        header("Location:" . base_url . "carrito/index");
+    }
+
+}
